45f2276743b7f5416715a617effeb7cfaa1d5354
Bob <wend3llsouza@gmail.com>
2021-05-16 17:33:04 -0300

    :package: Task | formato de uma tarefa

--- conexaoJavaSql/src/util/Conn.java
+++ conexaoJavaSql/src/util/Conn.java
@@ -3,19 +3,16 @@ package util;
 import java.sql.*;
 
 public class Conn {
-	public static Connection getConn() {
+	public static Connection getConn(){
 		Connection conn = null;
 		try {
 			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/connJava", "firstUser", "firstUser");
 			System.out.println("Connected");
+			
 		}catch(SQLException error) {
 			System.err.println("Have a beetle: " + error);
 		}
 		
 		return conn;
-	}
-	
-	public static void main(String[] args){
-		Conn.getConn();
 	}
 }
---
+++ conexaoJavaSql/src/util/InitConn.java
@@ -0,0 +1,7 @@
+package util;
+
+public class InitConn {
+	public static void main(String[] args){
+		Conn.getConn();
+	}
+}
