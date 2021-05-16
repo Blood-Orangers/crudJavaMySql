8302afbfddb58268b5579a43bd77e9068a18d225
Bob <wend3llsouza@gmail.com>
2021-05-16 17:03:21 -0300

    :package: Conexão MySql e Java usando mysql-conector jdbc

--- conexaoJavaSql/src/util/Conn.java
+++ conexaoJavaSql/src/util/Conn.java
@@ -1,9 +1,21 @@
 package util;
 
-import java.sql.Connection;
+import java.sql.*;
 
 public class Conn {
 	public static Connection getConn() {
 		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/connJava", "firstUser", "firstUser");
+			System.out.println("Connected");
+		}catch(SQLException error) {
+			System.err.println("Have a beetle: " + error);
+		}
+		
+		return conn;
+	}
+	
+	public static void main(String[] args){
+		Conn.getConn();
 	}
 }
